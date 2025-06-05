8acb6b8bf0d58d58cf59069d30c4bb8619306b1e
hoan.duong <hoan.duong@drjoy.vn>
2025-06-05 12:35:04 +0900

    refactor

--- src/main/java/com/drjoy/automation/service/SeleniumAutomationService.java
+++ src/main/java/com/drjoy/automation/service/SeleniumAutomationService.java
@@ -78,103 +78,11 @@ public class SeleniumAutomationService {
             throw new IllegalArgumentException("Invalid service class: " + className);
         }
 
+        // Process run test case
         AbstractTestSuite service = (AbstractTestSuite) clazz.getDeclaredConstructor().newInstance();
-        handle(service, selectedSettings);
-    }
-
-    private static void handle(AbstractTestSuite service, List<TeireiSetting> selectedSettings) {
         List<String> orderedSteps = service.getAllTestCase();
         PhaseProcessor.process(selectedSettings, setting ->
                 StaticExecutionRunner.runSteps(service.getClass(), setting, orderedSteps)
-        );
-    }
-
-    private static List<String> getAt0021Methods() {
-        return List.of("filterRequestsOnAT0021");
-    }
-
-    private static List<String> getAt0024Methods() {
-        return List.of("createSingleDayRequestOnAT0024B", "createPeriodRequestOnAT0024B");
-    }
-
-    private static List<String> getAt0026Methods() {
-        return List.of("editDayOffRequestReason", "addDayOffReason", "editOvertimeReason", "editResearchReason", "editWatchReason", "editDayOffWorkingReason", "editPreOvertimeReason", "editOtherReason");
-    }
-
-    private static List<String> getAt0029Methods() {
-        return List.of("exportLeaveBalance");
-    }
-
-    private static List<String> getAt0037Methods() {
-        return List.of("exportCSV");
-    }
-
-    private static List<String> getAt0038Methods() {
-        return List.of(
-                "editUserDetailAndSave",
-                "editUserDetailCol3AndSave"
-        );
-    }
-
-    private static List<String> getAt0047Methods() {
-        return List.of(
-                "AT_AT0047_1_2_5",
-                "AT_AT0047_1_3_6",
-                "half_time"
-        );
-    }
-
-    private static List<String> getAt0048Methods() {
-        return List.of("electUserForTimeOff");
-    }
-
-    private static List<String> getAt0049Methods() {
-        return List.of(
-                "searchByCurrentUsername",
-                "clickVacationHistoryButton",
-                "importFile"
-        );
-    }
-
-    private static List<String> getAt0050Methods() {
-        return List.of(
-                "grantLeaveType1",
-                "grantLeaveType2"
-        );
-    }
-
-    private static List<String> getAt0051Methods() {
-        return List.of(
-                "viewLeaveHistory",
-                "editDayOffAllocation",
-                "confirmFirstLeaveHistoryRow"
-        );
-    }
-
-    private static List<String> getAt0052Methods() {
-        return List.of(
-                "executeAT0052Flow",
-                "runHourlyPaidLeaveManagement"
-        );
-    }
-
-    private static List<String> getAt0053Methods() {
-        return List.of(
-                "checkForgetRequestOptions",
-                "checkLack5DayOffOptions"
-        );
-    }
-
-    private static List<String> getAt0064Methods() {
-        return List.of(
-                "searchAndClickUserDetail"
-        );
-    }
-
-    private static List<String> getAt0065Methods() {
-        return List.of(
-                "editUserDetailCol1AndSave1",
-                "editUserDetailCol1AndSave2"
         );
     }
 }
