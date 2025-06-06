7ac1a909488ac1c7ff586456308b4554f241e0a0
tien.luu <tien.luu@drjoy.vn>
2025-06-06 19:45:20 +0900

    Fix 22: Retry step - handle case locked screen

--- src/main/java/com/drjoy/automation/execution/StaticExecutionRunner.java
+++ src/main/java/com/drjoy/automation/execution/StaticExecutionRunner.java
@@ -1,5 +1,7 @@
 package com.drjoy.automation.execution;
 
+import com.drjoy.automation.config.Configuration;
+import com.drjoy.automation.config.DriverFactory;
 import com.drjoy.automation.execution.phase.PhaseSetting;
 import com.drjoy.automation.logging.TaskLoggerManager;
 
@@ -9,13 +11,19 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 import com.drjoy.automation.utils.WebUI;
+import com.drjoy.automation.utils.xpath.common.XpathCommon;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 @Slf4j
 public class StaticExecutionRunner {
 
     private static final Map<Class<?>, Map<String, Method>> methodMapCache = new HashMap<>();
     private static final int TOTAL_ATTEMPT = 2;
+
+    private StaticExecutionRunner() {}
 
     public static void runSteps(Class<?> serviceClass, PhaseSetting setting, List<String> orderedStepNames) {
         Map<String, Method> stepMethodMap = getCachedStepMethods(serviceClass, setting);
@@ -52,6 +60,15 @@ public class StaticExecutionRunner {
                     TaskLoggerManager.error("Phase {} failed after {} attempts", setting.getPhase(), TOTAL_ATTEMPT);
                 } else {
                     TaskLoggerManager.info("Retrying step {}...", stepName);
+
+                    // Xử lý confirm popup nếu có
+                    int retry = 0;
+                    while (retry < 5) {
+                        WebElement confirmBtn = WebUI.waitForElementClickable(By.xpath(XpathCommon.MODAL_CONFIRM_BTN.value), 1);
+                        if (confirmBtn == null) break;
+                        confirmBtn.click();
+                        retry++;
+                    }
                 }
                 ExecutionContext.clear();
             }
@@ -59,10 +76,23 @@ public class StaticExecutionRunner {
     }
 
     private static void detectUnexpectedError() {
-        if (WebUI.isMeetNotFoundPage()) {
+        boolean isNotFound = WebUI.isMeetNotFoundPage();
+        boolean isError = WebUI.isMeetErrorPage();
+        if (isNotFound) {
             TaskLoggerManager.error("Detected NOT FOUND page!");
-        } else if (WebUI.isMeetErrorPage()) {
-            TaskLoggerManager.error("Page is ERROR, maybe upstream or other reason. Please try again later!");
+        } else if (isError) {
+            TaskLoggerManager.error("Page is ERROR, maybe upstream or other reason. Please check it out!");
+        }
+
+        // Reload page when the screen locked
+        if (isError || isNotFound) {
+            WebDriver driver = DriverFactory.getDriver();
+            String targetURL = Configuration.getBaseUrl();
+
+            driver.get(targetURL);
+            WebUI.waitForConditionSucceed(driver.getCurrentUrl().equals(targetURL));
+
+            WebUI.waitForLoadingElement();
         }
     }
 
